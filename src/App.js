8164008aa3459a27171d6674cb772b991d83b9cc
Matt Wiens <mwiens91@gmail.com>
2019-09-13 23:40:43 -0700

    Add buttons to add/remove players

--- src/App.js
+++ src/App.js
@@ -16,22 +16,70 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.minPlayers = 5;
+    this.maxPlayers = 10;
+    this.state = {};
+
+    this.resetState();
+
+    this.addPlayer = this.addPlayer.bind(this);
+    this.removePlayer = this.removePlayer.bind(this);
+    this.resetState = this.resetState.bind(this);
+  }
+
+  resetState() {
     this.state = {
+      numPlayers: 5,
       players: ["player1", "player2", "player3", "player4", "player5"],
       playersLocked: false,
-      quests: [],
+      quests: [[{ missionLeader: 0 }]],
     };
+  }
+
+  addPlayer() {
+    // Don't add a player if the player number is maxed out
+    if (this.state.numPlayers >= this.maxPlayers) {
+      return;
+    }
+
+    this.setState({
+      numPlayers: this.state.numPlayers + 1,
+      players: [...this.state.players, `player${this.state.numPlayers + 1}`],
+    });
+  }
+
+  removePlayer() {
+    // Don't add a player if the player number is maxed out
+    if (this.state.numPlayers <= this.minPlayers) {
+      return;
+    }
+
+    this.setState({
+      numPlayers: this.state.numPlayers - 1,
+      players: this.state.players.slice(0, this.state.numPlayers - 1),
+    });
   }
 
   render() {
     return (
       <div className="App">
         <h1>Avalon vote tracker</h1>
-        <FontAwesomeIcon icon="lock" />
-        <FontAwesomeIcon icon="lock-open" />
-        <FontAwesomeIcon icon="plus" />
-        <FontAwesomeIcon icon="minus" />
-        <FontAwesomeIcon icon="redo" />
+
+        <div>
+          <FontAwesomeIcon icon="lock" />
+          <FontAwesomeIcon icon="lock-open" />
+
+          <button onClick={this.addPlayer}>
+            <FontAwesomeIcon icon="plus" /> add player
+          </button>
+
+          <button onClick={this.removePlayer}>
+            <FontAwesomeIcon icon="minus" /> remove player
+          </button>
+
+          <FontAwesomeIcon icon="redo" />
+        </div>
+
         <br />
 
         <table style={{ width: "100%" }}>
