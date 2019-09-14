f36cff32d6844a1b430c0e793aa5268a0040ebc3
Matt Wiens <mwiens91@gmail.com>
2019-09-13 20:53:15 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <h1>test</h1>
+        </div>
+    );
+  }
+}
+
+export default App;
