c4797ca192c00852c1de55c2eb1c336ce55e2018
Matt Wiens <mwiens91@gmail.com>
2019-09-13 22:04:02 -0700

    Remove react-window
    Wasn't exactly what I wanted.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import Grid from "./Grid";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Avalon vote tracker</h1>
-        <Grid />
       </div>
     );
   }
