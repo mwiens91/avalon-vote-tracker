782111f75854363809bcb00fb270a2f688e3c885
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-15 20:51:28 +0000

    Deploy mwiens91/avalon-vote-tracker to github.com/mwiens91/avalon-vote-tracker.git:gh-pages

---
+++ precache-manifest.b51ed5b2efc732ed195e47fb15b7f4bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54686f3d34452fdf1c6234ef551f8f0b",
+    "url": "/avalon-vote-tracker/index.html"
+  },
+  {
+    "revision": "a8422e66401ab4c38130",
+    "url": "/avalon-vote-tracker/static/css/main.d2579bcd.chunk.css"
+  },
+  {
+    "revision": "e5bb7da05ea6f1c23c82",
+    "url": "/avalon-vote-tracker/static/js/2.83d7fda4.chunk.js"
+  },
+  {
+    "revision": "a8422e66401ab4c38130",
+    "url": "/avalon-vote-tracker/static/js/main.0cf774b6.chunk.js"
+  },
+  {
+    "revision": "0bcada96e9622c33fab9",
+    "url": "/avalon-vote-tracker/static/js/runtime~main.5f4be305.js"
+  }
+]);
